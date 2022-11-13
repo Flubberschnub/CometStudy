4b1201c232035747bddf3800e5fa61ad734d4b89
Jonathan Lam <jonathan.kw.lam@gmail.com>
2022-11-12 22:30:29 -0600

    lists

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 function App() {
 	return (
+    /*
     <div>
       <img src={require('./CometStudy_map.png')} alt="not available"></img>
 
@@ -18,6 +19,29 @@ function App() {
 
       
     </div>
+    */
+    
+
+    <div>
+
+      <section class = "group-list">
+      <button onClick = {createCard}>Click</button>
+      <div class = "card" onClick = {on}>
+        <div class = "title">Group1</div>
+        <div class = "text">Group Details</div>
+          <p>Subjects: </p>
+          <p>Courses: </p>
+          <p>People: </p>
+        <img></img>
+      </div>
+      
+      </section>
+      <div class="overlay" onClick={off}>
+          <div id="text">Overlay Text</div>
+        </div>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    </div>
   
   )
 }
@@ -28,6 +52,32 @@ function on() {
 
 function off() {
   document.getElementsByClassName("overlay")[0].style.display = "none";
+}
+
+function createCard() {
+  
+  const newCard = document.createElement("div");
+  newCard.className = "card";
+  newCard.setAttribute("onClick", on);
+  
+  const newText = document.createElement("div");
+  newText.className = "text";
+  const newTitle = document.createElement("div"); 
+  newTitle.className = "title";
+  newTitle.innerHTML = "Group #";
+
+  const newSubject = document.createElement("p");
+  newSubject.innerHTML = "Subject";
+  newText.appendChild(newSubject);
+
+  newCard.appendChild(newTitle);
+  newCard.appendChild(newText);
+
+  const currentEl = document.getElementsByClassName("group-list")[0];
+  currentEl.appendChild(newCard);
+  
+    
+  
 }
 
 
