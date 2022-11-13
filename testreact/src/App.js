cb09cd65fe09eff8d710e8eb8d3c169105caa97e
Jonathan Lam <jonathan.kw.lam@gmail.com>
2022-11-12 18:49:44 -0600

    added image

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+/*import background from "./CometStudy_map.png";*/
 import './App.css';
+
+
 
 function App() {
 	return (
-  <div>
-    <p>yo1</p>
-  </div>
+    <div>
+      <img src={require('./CometStudy_map.png')} alt="not available"></img>
+
+      <div>
+        <div class="overlay" onClick={off}>
+          <div id="text">Overlay Text</div>
+        </div>
+        <button onClick={on}>Turn on overlay effect</button>
+      </div>
+
+      
+    </div>
+  
   )
 }
+
+function on() {
+  document.getElementsByClassName("overlay")[0].style.display = "block";
+}
+
+function off() {
+  document.getElementsByClassName("overlay")[0].style.display = "none";
+}
+
 
 export default App
