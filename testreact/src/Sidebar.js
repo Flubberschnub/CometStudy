4458ee53194a8655cc4c5c7f6a6055dce5a8fe73
Flubberschnub <kw6561@gmail.com>
2022-11-12 22:00:35 -0600

    search bar stuff

--- testreact/src/Sidebar.js
+++ testreact/src/Sidebar.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import './Sidebar.css';
+import App from './App';
+import {useFormik} from 'formik';
+import ReactDOM from 'react-dom/client';
+import {Helmet} from 'react-helmet';
 
-function App() {
+
+
+function Sidebar() {
+	const formik = useFormik({
+		initialValues: {
+			search: ""
+		},
+		onSubmit: (values, {resetForm}) => { 
+			// handle form submission
+			alert(values.search);
+			resetForm();
+		},
+		handleChange: values => {
+			// display search input
+		}
+	});
 	return (
 		<div>
-			<p>sidebar</p>
+			<div id="mySidenav" class="sidenav">
+				<button class="closebtn" onClick={closeNav}>&times;</button>
+				<form class="search-form" onSubmit={formik.handleSubmit}>
+					<input class="search-bar"
+						id="search"
+						name="search"
+						type="search"
+						placeholder="Search Study Section"
+						value={formik.values.search}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+					/>
+					<input class="search-button" type="submit" value="Submit" />
+				</form>
+			</div>
+
+			{/*Use any element to open the sidenav*/}
+			<button class="openbtn" onClick={openNav}>&equiv;</button>
+			{/*<span onclick="openNav()">open</span>*/}
+
+			{/*Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page*/}
+			<div id="main">
+				<div>{App()}</div>
+			</div>
+
+			
 		</div>
 	)
 }
 
-export default App
+export default Sidebar
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+	document.getElementById("mySidenav").style.width = "350px";
+	document.getElementById("main").style.marginLeft = "350px";
+}
+  
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+}
