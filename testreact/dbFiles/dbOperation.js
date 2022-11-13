29d7b6bcac0dbffa776445fad0f0af3675ea525b
KLin123 <karen234765@gmail.com>
2022-11-13 00:59:07 -0600

    things

--- testreact/dbFiles/dbOperation.js
+++ testreact/dbFiles/dbOperation.js
@@ -4,7 +4,7 @@ const config = require('./dbConfig'),
 const getTeams = async () => {
     try {
         let pool = await sql.connect(config);
-        let team = pool.request().query("SELECT * FROM makeTeam") // The semicolon may or may not be needed.
+        let team = pool.request().query("SELECT * FROM makeTeam")
         console.log(team);
         return(team);
     }
@@ -13,6 +13,32 @@ const getTeams = async () => {
     }
 }
 
+const getOneTeam = async (Team) => {
+    try {
+        let pool = await sql.connect(config);
+        let team = pool.request().query("SELECT * FROM makeTeam WHERE class = '${Team.classroom}' AND groupName = '${Team.groupName}'")
+        console.log(team);
+        return(team);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const createTeams = async (Team) => {
+    try {
+        let pool = await sql.connect(config);
+        let team = pool.request().query(`INSERT INTO makeTeam VALUES
+        ('${Team.groupName}', '${Team.class}', '${Team.section}', '${Team.building}', '${Team.classroom}', '${team.endTime}')
+        `) 
+        return(team);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    getTeams
+    getTeams,
+    createTeams
 }
