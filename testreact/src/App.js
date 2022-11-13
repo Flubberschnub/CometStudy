8b46250225f1aa84b59ed039dd185345c23376e4
KLin123 <103554703+KLin123@users.noreply.github.com>
2022-11-13 11:20:33 -0600

    Update App.js

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -1,5 +1,10 @@
+/*import { Console } from 'console';*/
+import {useLayoutEffect, useState} from 'react';
+import { render } from '@testing-library/react';
 import React from 'react';
-import {useState} from 'react';
+import {renderToStaticMarkup} from 'react-dom/server'
+import ReactDOM from 'react-dom/client';
+import Apptwo from './Sidebar';
 /*import background from "./CometStudy_map.png";*/
 import './App.css';
 import $ from 'jquery';
@@ -7,129 +12,161 @@ import $ from 'jquery';
 let count = 0;
 let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function App() {
 
-  const [returnedData, setReturnedData] = useState(['hello']);
-  const [group, setGroup] = useState({groupName: '', class: '', section: '', building: '', classroom: '', endTime: ''})
+let groupNameArr = ["GroupName1", "GroupName2", "GroupName3", "GroupName4", "GroupName5", "GroupName6", "GroupName7", "GroupName8", "GroupName9", "GroupName10",
+  "GroupName11", "GroupName12", "GroupName13", "GroupName14", "GroupName15", "GroupName16", "GroupName17", "GroupName18", "GroupName19", "GroupName20"];
+let groupSubjectArr = ["Computer Science", "Computer Engineering", "Biology", "Computer Science", "Computer Science", "Chemistry", "Computer Science",
+"Computer Science","Computer Science","Computer Science","Computer Science","Computer Science","Computer Science","Computer Science","Computer Science",
+"Computer Science","Computer Science","Computer Science","Computer Science","Computer Science"];
 
-  const setInput = (e) => {
-    const{name, value} = e.target;
-    console.log(value);
-    setGroup(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
+let dataArr = [
+  {
+    id: 0,
+    groupName: "study sesh",
+    subject: "CS2337.003",
+    location: "ECSW 2.301"
+  },
+  {
+    id: 1,
+    groupName: "lacker hackers",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
+  },
+  {
+    id: 2,
+    groupName: "gamergroup",
+    subject: "CS4337.HN1",
+    location: "ECSS 2.305"
+  },
+  {
+
+    id: 3,
+    groupName: "fun times",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
+  },
+  {
+
+    id: 4,
+    groupName: "hacker snackers",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
+  },
+  {
+
+    id: 5,
+    groupName: "comp sci group",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
+  },
+  {
+
+    id: 6,
+    groupName: "crammers",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
+  },
+  {
+
+    id: 7,
+    groupName: "exam prep",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
+  },
+  {
+
+    id: 8,
+    groupName: "learning time",
+    subject: "CS4337.HN1",
+    location: "ECSW 2.301"
   }
+]
 
-  const fetchData = async () => {
-    console.log(group);
-    const newData = await fetch('/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        name: group.groupName
-      })
-    })
-    .then(res => res.json())
-    console.log(newData);
-    setReturnedData(newData[0])
-  }
+var filteredArr = dataArr;
 
-	return (
+class App extends React.Component {
+
+  state = {
+    mssg: ""
+  };
+
+  handleButton = () => {
+    filteredArr = dataArr.filter(function (el){
+      return el.subject === "CS4337.HN1";
+    });
+    for(var i = 0; i < 20; i++){
+
+      const element = document.getElementById("groupCard");
+      element.remove();
+    }
+    const domNode = document.getElementById("clearbutton");
+    domNode.append({createAll});
     
-    /*
-    <div>
-      <img src={require('./CometStudy_map.png')} alt="not available"></img>
+    
+    this.setState({mssg:" "});
+    
+  };
 
+  render() {
+    
+    return (
+      /*
       <div>
-        <div class="overlay" onClick={off}>
-          <div id="text">Overlay Text</div>
+        <img src={require('./CometStudy_map.png')} alt="not available"></img>
+  
+        <div>
+          <div class="overlay" onClick={off}>
+            <div id="text">Overlay Text</div>
+          </div>
+          <button onClick={on}>Turn on overlay effect</button>
         </div>
-        <button onClick={on}>Turn on overlay effect</button>
+  
+        
       </div>
-
+      */
       
-    </div>
-    */
+  
+      <div onLoad = {createAll}>
+  
+        <div class = "group-list">
+          <button id = "clearbutton" onClick = {this.handleButton}>Click</button>
+          {/*<div class = "card" id = "0" onClick = {on}>
+            <div class = "title">Group 0</div>
+            <div class = "text">Group Details</div>
+              <p>Subjects: </p>
+              <p>Courses: </p>
+              <p>People: </p>
+            <img></img>
+    </div>*/}
+          {createAll}
+        
+        </div>
+  
+        <div class="overlay">
+            <div id="text">Group Name</div>
+            <div id="overlay-subject">Subject</div>
+            <div id="overlay-location">Location</div>
+        </div>
+  
+          <div>{this.state.mssg}</div>
+          <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      </div>
     
-    <div>
-  
-    <div className="addGroup">
-      <input 
-       name = "groupName" 
-       placeHolder = "groupName" 
-       onChange = {setInput}></input>
-      <input 
-       name = "class" 
-       placeHolder = "class" 
-       onChange = {setInput}>
-      </input>
-      <input 
-       name = "section"
-       placeHolder = "section"
-       onChange = {setInput}></input>
-      <input
-       name = "building" 
-       placeHolder = "building" 
-       onChange = {setInput}></input>
-      <input
-       name = "classroom" 
-       placeHolder = "classroom" 
-       onChange = {setInput}></input>
-      <input
-       name = "endTime" 
-       placeHolder = "endTime" 
-       onChange = {setInput}>
-      </input>
-      <button onClick = {() => fetchData()}>Create Group</button>
-      <p>groupName: {returnedData.groupName}</p>
-      <p>class: {returnedData.class}</p>
-      <p>section: {returnedData.section}</p>
-      <p>building: {returnedData.building}</p>
-      <p>classroom: {returnedData.classroom}</p>
-      <p>endTime: {returnedData.endTime}</p>
-    </div>
-
-      <section class = "group-list">
-      <button onClick = {createCard}>Click</button>
-      <button onClick = {initList}>Yay</button>
-      <div class = "card" id = "0" onClick = {on}>
-        <div class = "title">Group 0</div>
-        <div class = "text">Group Details</div>
-          <p>Subjects: </p>
-          <p>Courses: </p>
-          <p>People: </p>
-        <img></img>
-      </div>
-      
-      </section>
-      <div class="overlay">
-          <div id="text">Overlay Text</div>
-        </div>
-
-
-    </div>
-  
-  )
+    )
+    
+  }
 }
 
-function initList(){
-  alert("before");
 
-  $.get("process.php",
-      function(data)
-      {
-          alert(data);
-      } );
-  
-      alert("after");
+function filterArr(){
+  filteredArr = dataArr.filter(function (el){
+    return el.subject === "CS2337.003";
+  });
+  this.setState({mssg: "yo"});
 }
 
-function on(count) {
-  document.getElementsByClassName("overlay")[0].innerText = "The data for Group " + array[count];
+function on(text) {
+  document.getElementsByClassName("overlay")[0].innerText = "The data for Group " + text;
 }
 
 /*
@@ -137,8 +174,29 @@ function off() {
   document.getElementsByClassName("overlay")[0].style.display = "none";
 }
 */
+function handleClick(e){
+  document.getElementsByClassName("overlay")[0].innerText = `The data for Group ${e.groupName}:\n
+  ${e.subject}\n
+  ${e.location}`;
+  if(e.groupName == "hacker snackers"){
+    alert('thats us!');
+  }
+}
 
-function createCard() {
+var createAll = filteredArr.map((item) =>  {
+
+  return (
+    <div class = "card" key = {item.groupName} id = "groupCard" onClick = {() => handleClick(item)}>
+          <div class = "title">{item.groupName}</div>
+          <div class = "text">Group Details</div>
+            <p>Subjects: {item.subject}</p>
+            <p>Location: {item.location}</p>
+          <img></img>
+        </div>
+  )
+})
+
+function createCard(props) {
 
   count++;
   const newCard = document.createElement("div");
@@ -151,7 +209,9 @@ function createCard() {
   newText.className = "text";
   const newTitle = document.createElement("div"); 
   newTitle.className = "title";
-  newTitle.innerHTML = "Group " + count;
+  newTitle.innerHTML = props.name;
+
+  //const newTitle = props => React.createElement("div", null, props.text); 
 
   const newSubject = document.createElement("p");
   newSubject.innerHTML = "Subject";
